df5d14f846e34a86acb302c97ce96f52676ceb43
Kuroo-nekoo <phamtrihung5363@gmail.com>
2022-12-16 15:59:46 +0700

    Feat: add check coupon logic

--- payment.php
+++ payment.php
@@ -1,3 +1,13 @@
+<?php
+	$dummy = array("email" => "user1@gmail.com", "ngayDat" => "05/10/2001", "tongTien" => 50000, "maKM" => 1);
+	$dummy_film_info = array("tenPhim" => "SEMATIC ERROR: LỖI LOGIC 2D", "ngayChieu" => "Thứ 2, 01/09/2022", "gioChieu" => "9:30-11:20", "rapChieu" => "VIP 1, Phú Mỹ Hưng", "gheNgoi" => "D12, D13", "giaTien" => 12000, "soLuong" => 2);
+	$dummy_food_info = array(array("tenMon" => "Combo Bigmon", "soLuong" => 1, "giaTien" => 20000), array("tenMon" => "Combo Bigmon", "soLuong" => 1, "giaTien" => 20000));
+	$film_total = $dummy_film_info['giaTien'] * $dummy_film_info['soLuong'];
+	$food_total = 0;
+	include_once('./utils.php');
+?>
+	
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,10 +15,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./css/nav.css">
+	<script src="./payment.js"></script>
+	<link rel="stylesheet" href="./css/nav.css">
     <link rel="stylesheet" href="./css/homepage.css">
     <link rel="stylesheet" href="./css/general.css">
     <link rel="stylesheet" href="./css/card.css">
@@ -27,9 +38,9 @@
 		<div class="section">
 			<div class="container input_group">
 				<div style="margin-bottom: 20px;" class="text_bold">Thông tin khách hàng</div>
-				<input type="text" name="name" id="name" placeholder="Họ và tên"/>
 				<input type="email" name="mail" id="mail" placeholder="Email"/>
-				<input type="text" name="user" placeholder="Mã khuyến mãi" />
+				<input id="couponId" type="text" name="user" placeholder="Mã khuyến mãi" />
+				<button id="couponButton" class="apply_coupon_button">Thêm mã khuyến mãi</button>
 			</div>
 			
 			<div class="payment_method_group container">
@@ -77,37 +88,40 @@
 			<div class="d-flex receipt_info">
 				<img src="./img/17.jpg" alt="" class="film_img">
 				<div class="ml-4">
-					<div class="film_name">SEMATIC ERROR: LỖI LOGIC 2D</div>
+					<div class="film_name"><?php echo($dummy_film_info['tenPhim']) ?></div>
 					<table>
 						<tr>
 							<td>Ngày</td>
-							<td>Thứ 2, 01/09/2022</td>
+							<td><?php echo($dummy_film_info['ngayChieu']) ?></td>
 						</tr>
 						<tr>
 							<td>Giờ chiếu</td>
-							<td>9:30-11:20</td>
+							<td><?php echo($dummy_film_info['gioChieu']) ?></td>
 						</tr>
 						<tr>
 							<td>Rạp chiếu</td>
-							<td>D12, D13</td>
+							<td><?php echo($dummy_film_info['rapChieu']) ?></td>
 						</tr>
 						<tr>
 							<td>Ghế ngồi</td>
-							<td>Thứ 2, 01/09/2022</td>
+							<td><?php echo($dummy_film_info['gheNgoi']) ?></td>
 						</tr>
 						<tr>
-							<td>Số lượng: 02</td>
+							<td>Số lượng: <?php echo($dummy_film_info['soLuong']) ?></td>
 						</tr>
 						<tr>
-							<td>Giá: 180.000 VNĐ</td>
+							<td>Giá: <?php echo($dummy_film_info['giaTien']) ?> VNĐ</td>
 						</tr>
 					</table>
 					<div class="seperator" style="margin: 10px 0"></div>
-					<div class="info_group">
-						<div>Combo Bigmon(Ghi chú)</div>
-						<div>Số lượng: 01</div>
-					</div>
-					<div style="margin-top: 10px;">Giá: 120.000 VNĐ</div>
+					<?php foreach($dummy_food_info as $food_info):
+						$food_total += $food_info['giaTien'] * $food_info['soLuong']?>
+						<div class="info_group d-flex justify-content-between">
+							<div><?php echo(strval($food_info['tenMon'])) ?></div>
+							<div>Số lượng: <?php echo($food_info['soLuong']) ?></div>
+						</div>
+						<div style="margin-top: 10px;"><?php echo($food_info['giaTien']) ?> VNĐ</div>
+					<?php endforeach ?>
 					<div class="seperator" style="margin: 10px 0"></div>
 				</div>
 			</div>
@@ -117,8 +131,7 @@
 					Mã giảm giá
 				</div>
 				<div class="d-flex justify-content-around" style="margin: 80px 0;">
-					<div>Khuyến mãi</div>
-					<div>Mã cuối tuần</div>
+					
 				</div>
 			</div>
 			<div>
@@ -127,21 +140,20 @@
 				<div>
 					<div class="d-flex justify-content-between" style="margin-left:15px">
 						<div>Tạm tính:</div>
-						<div class="text_bold">300.000 VNĐ</div>
+						<div class="text_bold"><span id="temporaryPrice"><?php echo(strval($film_total + $food_total)) ?></span> VNĐ</div>
 					</div>
 					<div class="d-flex justify-content-between" style="margin-left:15px; margin-top:10px">
 						<div>Giảm giá:</div>
-						<div class="text_bold">-50.000 VNĐ</div>
+						<div class="text_bold"><span id="discountPrice">0</span> VNĐ</div>
 					</div>
 					<div class="seperator" style="margin: 20px 0"></div>
 					<div class="d-flex justify-content-between" style="margin-left:15px">
 						<div>Tổng tiền:</div>
-						<div class="text_bold" style="font-size: 20px;">250.000 VNĐ</div>
+						<div class="text_bold" style="font-size: 20px;"><span id="totalPrice"><?php echo strval($film_total + $food_total) ?></span> VNĐ</div>
 					</div>
 				</div>
 			</div>
 		</div>
-
 	</div>
 
 	<!-- Start footer -->
