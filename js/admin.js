1376fd8918d911ab6431ba78fdc13150506df23a
Kuroo-nekoo <phamtrihung5363@gmail.com>
2022-12-18 14:22:35 +0700

    Feat: add account admin

--- js/admin.js
+++ js/admin.js
@@ -39,20 +39,20 @@ $(document).ready(function () {
         deleteRow: "deleteRow",
         rowID: id,
         table: arr[arr.length - 1],
-        id: arr[arr.length -2]
+        id: arr[arr.length - 2],
       },
       success: function (response) {
         if (response == "success") {
           let row = document.getElementById("" + id);
-          row.parentNode.parentNode.parentNode.removeChild(row.parentNode.parentNode);
+          row.parentNode.parentNode.parentNode.removeChild(
+            row.parentNode.parentNode
+          );
         }
       },
     });
   });
 
-  
   $(".edit").focusout(function () {
-    
     let id = this.id;
     let split_id = id.split("_");
     let name = split_id[0];
@@ -60,60 +60,87 @@ $(document).ready(function () {
     let table = split_id[1];
     let value = $(this).text();
     let idDatabase = split_id[3];
-    
+
     console.log(name);
     console.log(value);
-    
-    if (name == "ten"){
+
+    if (name == "ten") {
       console.log(value);
       console.log(id);
       console.log(editID);
       console.log(table);
     }
-    
 
     $.ajax({
       url: "./page-admin/updateDataAdmin.php",
       type: "POST",
-      data: {table: table, value: value, id: editID,name: name, idDatabase: idDatabase},
-        success: function(response){
+      data: {
+        table: table,
+        value: value,
+        id: editID,
+        name: name,
+        idDatabase: idDatabase,
+      },
+      success: function (response) {
         alert("Updated Success!");
-      }
-    })
-
+      },
+    });
   });
-
 });
 
+// function updateDataAccount(id,col){
+//   console.log(id);
+//   console.log(col);
+//   ID = id;
+//   ROW = col;
+//   $(".modal-body #email").val(ID);
+// }
 
-  // function updateDataAccount(id,col){
-  //   console.log(id);
-  //   console.log(col);
-  //   ID = id;
-  //   ROW = col; 
-  //   $(".modal-body #email").val(ID);
-  // }
+// function updateAccount(){
 
-  // function updateAccount(){
-    
-  //   $.ajax({
-  //     type: "post",
-  //     url: "./page-admin/deleteData.php",
-  //     data: {
-  //       updateRowAccount: "updateRow",
-  //       email: $("#email").val().trim(),
-  //       ten: $("#ten").val().trim(),
-  //       matKhau: $("#matKhau").val().trim(),
-  //       sdt: $("#sdt").val().trim(),
-  //       admin: $("input[name=admin]:checked").val().trim()
-  //     },
-  //     success: function (response) {
-  //       if (response == "success") {
-  //         let row = ($("#"+ROW));
-  //         console.log(row);
-  //       }
-  //     },
-  //   });
-  // }
+//   $.ajax({
+//     type: "post",
+//     url: "./page-admin/deleteData.php",
+//     data: {
+//       updateRowAccount: "updateRow",
+//       email: $("#email").val().trim(),
+//       ten: $("#ten").val().trim(),
+//       matKhau: $("#matKhau").val().trim(),
+//       sdt: $("#sdt").val().trim(),
+//       admin: $("input[name=admin]:checked").val().trim()
+//     },
+//     success: function (response) {
+//       if (response == "success") {
+//         let row = ($("#"+ROW));
+//         console.log(row);
+//       }
+//     },
+//   });
+// }
 
+function addAccount() {
+  let ten = $("#ten").val().trim();
+  let sdt = $("#sdt").val().trim();
+  let admin = $("#admin").is(":checked") ? 1 : 0;
+  let matKhau = $("#matKhau").val.trim();
+  let email = $("email").val().trim();
 
+  $.ajax({
+    type: "post",
+    url: "./page-admin/addDataAdmin.php",
+    data: {
+      target: "taikhoan",
+      ten,
+      sdt,
+      admin,
+      matKhau,
+      email,
+    },
+    success: function (response) {
+      if (response == "success") {
+        let row = $("#" + ROW);
+        console.log(row);
+      }
+    },
+  });
+}
